555f35b607c1a98228b8ed475c4c24abf795314d
jayer <jayer.ltl@outlook.com>
2021-10-27 02:46:31 +0800

    Add files via upload

---
+++ Chapter 8 贪婪算法/8.3 集合覆盖问题/approximation.py
@@ -0,0 +1,26 @@
+states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
+
+stations = {}
+stations['kone'] = set(['id', 'nv', 'ut'])
+stations['ktwo'] = set(['wa', 'id', 'mt'])
+stations['kthree'] = set(['or', 'nv', 'ca'])
+stations['kfour'] = set(['nv', 'ut'])
+stations['kfive'] = set(['ca', 'az'])
+
+final_stations = set()  #存储最终选择的广播台
+
+
+
+while states_needed:
+    best_station = None #存储覆盖最多未覆盖州的广播台
+    states_covered = set()  #包含广播台覆盖的所有未覆盖的州
+    for station, states_for_station in stations.items():
+        covered = states_needed & states_for_station # 计算交集用 &， 并集 |，差集 -
+        if len(covered) > len(states_covered):
+            best_station = station
+            states_covered = covered
+
+    states_needed -= states_covered
+    final_stations.add(best_station)
+
+print(final_stations)
