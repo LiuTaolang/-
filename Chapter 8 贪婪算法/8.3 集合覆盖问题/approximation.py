f26d7c4c09d395a8a4db833df9c03512ff6f0681
jayer <jayer.ltl@outlook.com>
2021-11-09 01:29:08 +0800

    回答8.3集合覆盖问题的作业‘

--- Chapter 8 贪婪算法/8.3 集合覆盖问题/approximation.py
+++ Chapter 8 贪婪算法/8.3 集合覆盖问题/approximation.py
@@ -13,7 +13,7 @@ final_stations = set()  #存储最终选择的广播台
 
 while states_needed:
     best_station = None #存储覆盖最多未覆盖州的广播台
-    states_covered = set()  #包含广播台覆盖的所有未覆盖的州
+    states_covered = set()  #包含广播台覆盖的所有未覆盖的州 #set()创建一个无序不重复的元素集，可进行关系测试，删除重复数据，还可以计算交集、差集、并集等。
     for station, states_for_station in stations.items():
         covered = states_needed & states_for_station # 计算交集用 &， 并集 |，差集 -
         if len(covered) > len(states_covered):
